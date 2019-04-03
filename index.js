ccaaa02c5009b424a86eabb2a30c63808f2b6994
Cory Henderson <chenderson19899@gmail.com>
2019-04-02 18:28:30 -0700

    updates index

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 const PORT = 8080;
 
 app.use(express.static('./index.html'));
+app.get('*', (req, res) => {
+  res.render('./index.html');
+})
 
 app.listen(PORT, () => {
   console.log('Web Server up on port', PORT);
