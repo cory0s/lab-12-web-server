cc2ab43c870a344bc3c35adc873579972ad509e8
Cory Henderson <chenderson19899@gmail.com>
2019-04-02 18:35:24 -0700

    index

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 const express = require('express');
 const app = express();
-const PORT = 8080;
+// const PORT = 8080;
 
 app.use(express.static('./index.html'));
 app.get('*', (req, res) => {
   res.render('./index.html');
 })
 
-app.listen(PORT, () => {
-  console.log('Web Server up on port', PORT);
-});
+// app.listen(PORT, () => {
+//   console.log('Web Server up on port', PORT);
+// });
